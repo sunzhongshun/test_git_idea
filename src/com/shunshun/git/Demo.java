c5e8ba6b159f82dc019d1affbff5aca5a2daf7d1
sunzhongshun <zhongshun_sun@126.com>
2019-10-30 14:49:53 +0800

    git 20191030-szs 提交到本地仓库

---
+++ src/com/shunshun/git/Demo.java
@@ -0,0 +1,13 @@
+package com.shunshun.git;
+
+/**
+ * @author 孙忠顺
+ * @create 2019-10-30 14:49
+ */
+public class Demo {
+
+	public static void main(String[] args){
+		System.out.println("demo");
+	}
+
+}
